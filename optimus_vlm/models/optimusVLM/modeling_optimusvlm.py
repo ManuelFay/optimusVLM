f15ee3597dc00a894633d90c8c6170065bf98186
ManuelFay <manuel.faysse@illuin.tech>
2024-12-09 13:28:48 +0100

    simplify

--- optimus_vlm/models/optimusVLM/modeling_optimusvlm.py
+++ optimus_vlm/models/optimusVLM/modeling_optimusvlm.py
@@ -1,23 +1,10 @@
-from transformers import Idefics3Model
-
-from optimus_vlm.models.optimus import OptimusModel
+from transformers import Idefics3Model, Idefics3ForConditionalGeneration
 
 
 class OptimusVLM(Idefics3Model):
     def __init__(self, config):
         super(OptimusVLM, self).__init__(config=config)
-        self.model: Idefics3Model = Idefics3Model(config)
 
-    def forward(self, *args, **kwargs):
-        return self.model(*args, **kwargs)
-
-
-class OptimusVLMForMLM(OptimusVLM):
+class OptimusVLMForMLM(Idefics3ForConditionalGeneration):
     def __init__(self, config):
         super(OptimusVLMForMLM, self).__init__(config=config)
-        self.mlm = True
-        self.lm_head = self.model.lm_head
-
-    def forward(self, *args, **kwargs):
-        hidden_states = self.model(*args, **kwargs)
-        return self.lm_head(hidden_states)
